f6f83a60f15cecc3d8d634ba76a88cc9d4476e84
csoftware-arigpt <ksoarigpt@gmail.com>
2026-05-03 13:25:52 +0300

    fix(nix): expose GTK3 gsettings schemas in dev shell
    Add wrapGAppsHook3, glib, gsettings-desktop-schemas, and a shellHook
    that prepends gtk3 + gsettings-desktop-schemas schema dirs to
    XDG_DATA_DIRS so org.gtk.Settings.FileChooser resolves at startup.
    Fixes Trace/breakpoint trap on `python3 -m cuetie`.

--- shell.nix
+++ shell.nix
@@ -1,11 +1,26 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    wrapGAppsHook3
+    gobject-introspection
+  ];
   buildInputs = with pkgs; [
     python313Packages.pygobject3
     python313Packages.mutagen
     python313Packages.charset-normalizer
     gtk3
-    gobject-introspection
+    glib
+    gsettings-desktop-schemas
     ffmpeg
   ];
+  shellHook = ''
+    for d in \
+      ${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name} \
+      ${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}; do
+      case ":$XDG_DATA_DIRS:" in
+        *":$d:"*) ;;
+        *) export XDG_DATA_DIRS="$d:$XDG_DATA_DIRS" ;;
+      esac
+    done
+  '';
 }
